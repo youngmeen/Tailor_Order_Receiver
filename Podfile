0bd5dd5cc87073db5777ef6cde8220ef61d4ca6f
developer <youngmeen101@sgrsoft.com>
2021-07-05 10:12:18 +0900

    first_init

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TailorOrder_Receive' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TailorOrder_Receive
+
+end
---
+++ TailorOrder_Receive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TailorOrder_Receive
+//
+//  Created by developer on 2021/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
